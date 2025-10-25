e2e73dc821abd7c15cb4a9e54d356904f577cdbc
Andre Pires <andreeepiresss1@gmail.com>
2025-10-25 15:12:19 +0100

    Solved lab

---
+++ lab-react-discord-hero/src/Components/Buttons.jsx
@@ -0,0 +1,8 @@
+export default function Buttons(){
+ return(
+    <div className="bothButtons">
+        <button id="buttom1">Download for Mac</button>
+        <button id="buttom2">Open Discord in your browser</button>
+    </div>       
+    )
+}
